d45301424996fd9f1030b665eb5c94a209756058
Ruksana <ruksana.b@indivillage.in>
2025-11-07 11:54:55 +0530

    Changing the gem speed

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,7 +7,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject obstaclePrefab;
     public GameObject gemPrefab;
     private Vector3 spawnPosO = new Vector3(0, 0, 25);
-    private Vector3 spawnPosG = new Vector3(0, 1, 25);
+    private Vector3 spawnPosG = new Vector3(0, 1, 30);
     private float startDelay = 2;
     private float repeatRate = 2;
     // Start is called before the first frame update
