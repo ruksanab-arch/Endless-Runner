665e29e74129c70f62b53c9ec53192c1adf48050
Ruksana <ruksana.b@indivillage.in>
2025-11-10 14:25:31 +0530

    Adding economy coin logic using playerpref

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    public static ScoreManager Instance { get; private set; }
     // Start is called before the first frame update
-    void Start()
+    private const string COINS_KEY = "PlayerCoins";
+    public int Coins { get; private set; }
+
+    [Header("UI Reference (Optional)")]
+    public TextMeshProUGUI coinText; // Drag UI text here in Inspector
+
+    private void Awake()
     {
-        
+        // Singleton pattern (only one instance)
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadCoins();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        UpdateCoinUI();
+    }
+
+    public void AddCoins(int amount)
+    {
+        Coins += amount;
+        SaveCoins();
+        UpdateCoinUI();
+    }
+
+    public void SaveCoins()
+    {
+        PlayerPrefs.SetInt(COINS_KEY, Coins);
+        PlayerPrefs.Save();
+    }
+
+    public void LoadCoins()
+    {
+        Coins = PlayerPrefs.GetInt(COINS_KEY, 0);
+    }
+
+    private void UpdateCoinUI()
+    {
+        if (coinText != null)
+            coinText.text = "Coins: " + Coins.ToString();
     }
 }
