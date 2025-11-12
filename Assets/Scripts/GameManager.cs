02fbab1aef541bcf08017b457188eb235486a3d4
Ruksana <ruksana.b@indivillage.in>
2025-11-12 16:48:41 +0530

    Add Attempts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    [Header("Attempts Settings")]
+    public int MaxAttempts = 3;
+    public int RemainingAttempts;
+
+    [Header("UI")]
+    public TextMeshProUGUI AttemptsText;
+    public GameObject gameOverPanel;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+            Instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject); // Persist across scenes
+        RemainingAttempts = MaxAttempts;
+        UpdateAttemptsUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPlayerDeath()
     {
-        
+        RemainingAttempts--;
+        UpdateAttemptsUI();
+
+        if (RemainingAttempts > 0)
+        {
+            Debug.Log("Attempts left: " + RemainingAttempts);
+            // Restart level after 1 second
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            Debug.Log("No attempts left! Game Over.");
+            if (gameOverPanel != null)
+                gameOverPanel.SetActive(true);
+        }
+    }
+
+    private void UpdateAttemptsUI()
+    {
+        if (AttemptsText != null)
+            AttemptsText.text = "Attempts: " + RemainingAttempts;
+    }
+
+    public void ResetAttempts()
+    {
+        RemainingAttempts = MaxAttempts;
+        UpdateAttemptsUI();
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(false);
     }
 }
