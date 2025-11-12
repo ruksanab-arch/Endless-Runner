fc4e22a4153c88ac46935d062338f89c04e62f61
Ruksana <ruksana.b@indivillage.in>
2025-11-12 14:32:31 +0530

    Set entry free to 20 gems

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,23 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 public class MainMenuManager : MonoBehaviour
 {
+    [Header("Scene Settings")]
     [SerializeField] private string gameplaySceneName = "GameScene";
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+    [Header("Entry Fee Settings")]
+    [SerializeField] private int entryFee = 20;
 
-    }    
-     public void OnPlayButton()
+    [Header("UI References")]
+    [SerializeField] private TextMeshProUGUI messageText; // Optional: show warning message
+
+    public void OnPlayButton()
     {
-        SceneManager.LoadScene(gameplaySceneName);
+        // Ensure ScoreManager exists
+        if (ScoreManager.Instance == null)
+        {
+            Debug.LogError("ScoreManager not found in scene!");
+            return;
+        }
+
+        // Check if player has enough gems
+        if (ScoreManager.Instance.HasEnoughGems(entryFee))
+        {
+            // Deduct entry fee
+            ScoreManager.Instance.SpendGems(entryFee);
+
+            // Load gameplay scene
+            SceneManager.LoadScene(gameplaySceneName);
+        }
+        else
+        {
+            // Not enough gems
+            if (messageText != null)
+            {
+                messageText.text = "Not enough gems to play! (Need 20)";
+                messageText.color = Color.red;
+            }
+            else
+            {
+                Debug.Log("Not enough gems to play! (Need 20)");
+            }
+        }
     }
-     
 }
